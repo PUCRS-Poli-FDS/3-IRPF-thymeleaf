ecfa8aeda4932130972ce298dd612813412bd5b7
Mario Specht <ms.specht@gmail.com>
2019-10-08 20:31:36 -0300

    Refatorado importações

--- src/test/java/br/pucrs/irpf/IrpfApplicationTests.java
+++ src/test/java/br/pucrs/irpf/IrpfApplicationTests.java
@@ -8,15 +8,10 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-//@SpringBootTest
-//@ComponentScan(basePackages ={"br.pucrs.irpf"})
 @ContextConfiguration(classes = {IrpfApplication.class})
 public class IrpfApplicationTests {
 
